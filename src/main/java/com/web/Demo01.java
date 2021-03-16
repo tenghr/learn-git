1134caf19b89780a514b9dbfc8de9706411094c4
滕浩然 <tenghaoran98@foxmail.com>
2021-03-16 20:10:49 +0800

    update src/main/java/com/web/Demo01.java. 制造一个冲突

--- src/main/java/com/web/Demo01.java
+++ src/main/java/com/web/Demo01.java
@@ -4,5 +4,6 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("git测试");
         System.out.println("git测试2");
+        System.out.println("我是冲突")
     }
 }
